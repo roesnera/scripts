031f0ac1eebe98936c735b72f8faceba1e7570bd
Adam Roesner <aaroesner@gmail.com>
2025-07-17 18:27:22 -0400

    Update slack starting script, add sock listener

--- slack_start.sh
+++ slack_start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
-slack=$(ps a | awk '{ print $5 }' | rg ^slack$)
-
-echo $slack
+function handle {
+  notify-send "handling socket emission"
+  slack=$(ps a | awk '{ print $5 }' | rg ^slack$);
+  if [ -z $slack ]; then
+    slack
+    notify-send "starting slack"
+  else
+    notify-send "slack already running"
+  fi
+}
+handle
